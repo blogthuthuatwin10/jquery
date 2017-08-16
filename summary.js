92b1e221d851ea8ece5b03eb0e7ec5767d9501b6
Nguyễn Tuấn <nguyenanhtuan2401@gmail.com>
2017-08-16 19:52:56 +0700

    Update summary.js

--- summary.js
+++ summary.js
@@ -2,7 +2,7 @@
 
 var summary = 76;
 
-jQuery(document)["ready"](function($){$(window)["load"](function(){$(".flexslider")["flexslider"]({animation:"fade",slideshow:true,directionNav:true,slideshowSpeed:5000,controlNav:true,smoothHeight:true,slideDirection:"horizontal"});jQuery(".slides")["addClass"]("loaded")});var aboveHeight=$("#leader-wrapper")["outerHeight"]();$(window)["scroll"](function(){if($(window)["scrollTop"]()>200){$("#navigation")["addClass"]("fixed-nav")["css"]("top","0")["next"]()["css"]("padding-top","43px")}else {$("#navigation")["removeClass"]("fixed-nav")["next"]()["css"]("padding-top","0")}})});
+jQuery(document)["ready"](function($){$(window)["load"](function(){$(".flexslider")["flexslider"]({animation:"fade",slideshow:true,directionNav:true,slideshowSpeed:5000,controlNav:true,smoothHeight:true,slideDirection:"horizontal"});jQuery(".slides")["addClass"]("loaded")});var aboveHeight=$("#leader-wrapper")["outerHeight"]();$(window)["scroll"](function(){if($(window)["scrollTop"]()>200){$("#navigation")["removeClass"]("fixed-nav")["css"]("top","0")["next"]()["css"]("padding-top","43px")}else {$("#navigation")["addClass"]("fixed-nav")["next"]()["css"]("padding-top","0")}})});
 
 
 function stripHtmlTags(e, r) {
